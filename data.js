27e44cd6066220733cf5a191983389a8f83d20a6
Jesux011 <gsrtitk@gmail.com>
2025-08-06 23:32:42 -0300

    Update data.js

---
+++ data.js
@@ -0,0 +1,84 @@
+// data.js
+const recursos = [
+    {
+        id: 1,
+        nombre: "Resident Evil 4",
+        tipo: "juego",
+        descripcion: "Un clásico del survival horror remasterizado. Leon S. Kennedy debe rescatar a la hija del presidente.",
+        requisitos: "SO: Windows 10<br>Procesador: Intel Core i5-7500<br>Memoria: 8 GB RAM<br>Gráficos: GTX 1050 Ti<br>Almacenamiento: 50 GB",
+        downloads: 5320,
+        rating: "⭐⭐⭐⭐⭐",
+        comments: ["Excelente remake!", "Gráficos 4K increíbles", "Un poco pesado en mi PC"],
+        links: {
+            direct: "https://gofile.io/d/abc123",
+            mediafire: "https://www.mediafire.com/file/xyz"
+        },
+        imagen: "Images/Resi4-gc-cover.jpg"
+    },
+    {
+        id: 2,
+        nombre: "Dead Island",
+        tipo: "juego",
+        descripcion: "Supervivencia en una isla infestada de zombis. Combate cuerpo a cuerpo brutal.",
+        requisitos: "SO: Windows 7 / Vista / XP<br>Procesador: Core2Duo 2.66 GHz<br>Memoria: 1 GB RAM<br>Gráficos: GeForce 8600 GT / Radeon HD 2600XT (256 MB VRAM)<br>Almacenamiento: 7 GB",
+        downloads: 8740,
+        rating: "⭐⭐⭐⭐☆",
+        comments: ["Divertido pero bugueado", "Me encanta el combate", "Recomendado para fans de left 4 dead 2"],
+        links: {
+            direct: "https://gofile.io/d/def456",
+            mediafire: "https://www.mediafire.com/file/uvw"
+        },
+        imagen: "Images/Dead_island_PC_packshot.png"
+    },
+    {
+        id: 3,
+        nombre: "Postal 2",
+        tipo: "juego",
+        descripcion: "Juego de disparos en primera persona satírico y violento con mundo abierto y misiones absurdas.",
+        requisitos: "SO: Windows XP / Vista / 7 / 8 / 10<br>Procesador: Pentium III o Athlon a 1.2 GHz<br>Memoria: 384 MB RAM<br>Gráficos: DirectX 8.1 compatible con al menos 64 MB<br>Almacenamiento: 4 GB",
+        downloads: 3200,
+        rating: "⭐⭐⭐⭐☆",
+        comments: [
+            "Juegazo xdd lo jugaba de niño",
+            "Obra maestra xd"
+        ],
+        links: {
+            direct: "https://gofile.io/d/ghi789",
+            mediafire: "https://www.mediafire.com/file/qrs"
+        },
+        imagen: "Images/Postal_2_cover.png"
+    },
+    {
+        id: 4,
+        nombre: "Left 4 Dead 2",
+        tipo: "juego",
+        descripcion: "Juego cooperativo de disparos en primera persona contra hordas de zombis con acción rápida y teamwork.",
+        requisitos: "SO: Windows XP / Vista / 7<br>Procesador: Intel Core 2 Duo 2.4 GHz<br>Memoria: 2 GB RAM<br>Gráficos: NVIDIA GeForce 7600 / ATI Radeon X800<br>Almacenamiento: 13 GB libre",
+        downloads: 12500,
+        rating: "⭐⭐⭐⭐⭐",
+        comments: [
+            "Juegazo del ciber",
+            "Mejor juego de zombies"
+        ],
+        links: {
+            direct: "https://gofile.io/d/jkl012",
+            mediafire: "https://www.mediafire.com/file/tuv"
+        },
+        imagen: "Images/Left4Dead2.jpg"
+    },
+    {
+        id: 20,
+        nombre: "Tony Hawks Underground 2",
+        tipo: "juego",
+        descripcion: "Juego de skate con historia loca y humor absurdo donde viajas por el mundo haciendo trucos extremos.",
+        requisitos: "SO: Windows 98/ME/2000/XP<br>Procesador: Pentium III 1.0 GHz<br>Memoria: 256 MB RAM<br>Gráficos: 64 MB (GeForce 3 o superior)<br>Almacenamiento: 1.8 GB libres",
+        downloads: 0,
+        rating: "⭐⭐⭐⭐☆",
+        comments: ["Divertido Pero con mods mas", "Me encanta el combate", "Recomendado para fans de left 4 dead 2"],
+        links: {
+            direct: "https://gofile.io/d/def456",
+            mediafire: "https://www.mediafire.com/file/uvw"
+        },
+        imagen: "Images/Tony.png"
+    }
+];
