0f2e2e95149a554fcddb828e5305238fc65777e8
Games <gsrtitk@gmail.com>
2025-08-08 18:48:10 -0300

    Update data.js

--- data.js
+++ data.js
@@ -10,8 +10,8 @@ const recursos = [
         rating: "⭐⭐⭐⭐⭐",
         comments: ["Excelente Juego!", "Gráficos decentes increíbles", "el mejor resident evil viejo"],
         links: {
-            direct: "https://gofile.io/d/x9dhme    ",
-            mediafire: "https://gofile.io/d/x9dhme    "
+            direct: "https://gofile.io/d/x9dhme",
+            mediafire: "https://gofile.io/d/x9dhme"
         },
         imagen: "Images/Resi4-gc-cover.jpg",
         password: "123"
@@ -26,8 +26,8 @@ const recursos = [
         rating: "⭐⭐⭐☆☆",
         comments: ["Divertido pero bugueado", "Me encanta el combate", "Recomendado para fans de left 4 dead 2"],
         links: {
-            direct: "https://gofile.io/d/78ptPe    ",
-            mediafire: "https://gofile.io/d/78ptPe    "
+            direct: "https://gofile.io/d/78ptPe",
+            mediafire: "https://gofile.io/d/78ptPe"
         },
         imagen: "Images/Dead_island_PC_packshot.png",
         password: "123"
@@ -45,8 +45,8 @@ const recursos = [
             "Obra maestra xd"
         ],
         links: {
-            direct: "https://gofile.io/d/avMKZl    ",
-            mediafire: "https://gofile.io/d/avMKZl    "
+            direct: "https://gofile.io/d/avMKZl",
+            mediafire: "https://gofile.io/d/avMKZl"
         },
         imagen: "Images/Postal_2_cover.png",
         password: "123"
@@ -64,62 +64,62 @@ const recursos = [
             "Mejor juego de zombies"
         ],
         links: {
-            direct: "https://gofile.io/d/Js6Zsk    ",
-            mediafire: "https://gofile.io/d/Js6Zsk    "
+            direct: "https://gofile.io/d/Js6Zsk",
+            mediafire: "https://gofile.io/d/Js6Zsk"
         },
         imagen: "Images/Left4Dead2.jpg",
         password: "123"
     },
     {
-        id: 20,
-        nombre: "Call of Duty Black ops 1",
+        id: 5,
+        nombre: "Call of Duty: Black Ops 1",
         tipo: "juego",
         descripcion: "Un shooter en primera persona ambientado en la Guerra Fría, lleno de acción, espionaje y misiones secretas.",
-        requisitos: "So: Windows XP/Vista/7<br>Procesador: Intel Core2 Duo E6600 / AMD Phenom X3 8750<br>RAM: 2 GB<br>Gráficos: NVIDIA GeForce 8600GT / ATI Radeon X1950Pro<br>Almacenamiento: 12 GB",
+        requisitos: "SO: Windows XP/Vista/7<br>Procesador: Intel Core2 Duo E6600 / AMD Phenom X3 8750<br>RAM: 2 GB<br>Gráficos: NVIDIA GeForce 8600GT / ATI Radeon X1950Pro<br>Almacenamiento: 12 GB",
         downloads: 0,
         rating: "⭐⭐⭐⭐⭐",
         comments: ["Divertido Pero con mods mas", "Me encanta el combate", "Recomendado para fans de left 4 dead 2"],
         links: {
-            direct: "https://gofile.io/d/Qax9uu    ",
-            mediafire: "https://gofile.io/d/Qax9uu    "
+            direct: "https://gofile.io/d/Qax9uu",
+            mediafire: "https://gofile.io/d/Qax9uu"
         },
         imagen: "Images/CoD_Black_Ops_cover.png",
         password: "123"
     },
     {
-        id: 13,
+        id: 6,
         nombre: "Red Dead Redemption 1",
         tipo: "juego",
-        descripcion: "un exforajido obligado por el gobierno a cazar a su antigua banda para recuperar a su familia en el viejo oeste de 1911.",
+        descripcion: "Un exforajido obligado por el gobierno a cazar a su antigua banda para recuperar a su familia en el viejo oeste de 1911.",
         requisitos: "SO: Windows 10 64‑bit<br>Procesador: Intel Core i5‑4670 / AMD FX‑9590<br>Memoria: 8 GB RAM<br>Gráficos: NVIDIA GeForce GTX 960 / AMD Radeon R7 360<br>Almacenamiento: 12 GB<br>DirectX: 12<br>SSD recomendado",
         downloads: 0,
         rating: "⭐⭐⭐⭐⭐",
         comments: ["Divertido", "Me encanta Los caballos", "Recomendado para fans de vaqueros"],
         links: {
-            direct: "https://gofile.io/d/c2B4Cd    ",
-            mediafire: "https://gofile.io/d/c2B4Cd    "
+            direct: "https://gofile.io/d/c2B4Cd",
+            mediafire: "https://gofile.io/d/c2B4Cd"
         },
         imagen: "Images/Red_Dead_Redemption.jpg",
         password: "123"
     },
     {
-        id: 14,
+        id: 7,
         nombre: "Payday 2",
         tipo: "juego",
-        descripcion: "un grupo de criminales que realiza asaltos, robos y misiones cooperativas en primera persona, donde el sigilo o la acción afectan el resultado.",
+        descripcion: "Un grupo de criminales que realiza asaltos, robos y misiones cooperativas en primera persona, donde el sigilo o la acción afectan el resultado.",
         requisitos: "SO: Windows 7<br>Procesador: 2 GHz Dual Core<br>Memoria: 4 GB RAM<br>Gráficos: NVIDIA GeForce 8800 / ATI Radeon HD 2600 (512 MB VRAM)<br>Almacenamiento: 31 GB",
         downloads: 0,
         rating: "⭐⭐⭐⭐⭐",
         comments: ["Juegazo", "XD"],
         links: {
-            direct: "https://www.mediafire.com/file/y7z5pibt4j1ol9q/PayDay.2.v1.143.246-Repack.iso.torrent/file    ",
-            mediafire: "https://www.mediafire.com/file/y7z5pibt4j1ol9q/PayDay.2.v1.143.246-Repack.iso.torrent/file    "
+            direct: "https://www.mediafire.com/file/y7z5pibt4j1ol9q/PayDay.2.v1.143.246-Repack.iso.torrent/file",
+            mediafire: "https://www.mediafire.com/file/y7z5pibt4j1ol9q/PayDay.2.v1.143.246-Repack.iso.torrent/file"
         },
         imagen: "Images/Payday2cover.jpg",
         password: "contraseña123"
     },
     {
-        id: 15,
+        id: 8,
         nombre: "Battlefield 2",
         tipo: "juego",
         descripcion: "Juego de disparos en primera persona con combates a gran escala, vehículos y mapas grandes ambientado en una guerra ficticia moderna.",
@@ -128,14 +128,14 @@ const recursos = [
         rating: "⭐⭐⭐⭐☆",
         comments: ["Clásico de los shooters", "Épicas batallas con vehículos"],
         links: {
-            direct: "https://gofile.io/d/DDlRTY    ",
-            mediafire: "https://gofile.io/d/DDlRTY    "
+            direct: "https://gofile.io/d/DDlRTY",
+            mediafire: "https://gofile.io/d/DDlRTY"
         },
         imagen: "Images/51XE9qx04ML._UF1000,1000_QL80_.jpg",
         password: "123"
     },
     {
-        id: 16,
+        id: 9,
         nombre: "God of War (2018)",
         tipo: "juego",
         descripcion: "Kratos y su hijo Atreus emprenden un viaje en busca de la respuesta de la esposa de Kratos, en una mitología nórdica brutal y emocional.",
@@ -144,14 +144,14 @@ const recursos = [
         rating: "⭐⭐⭐⭐⭐",
         comments: ["Obra maestra del storytelling", "Combate brutal y emocional"],
         links: {
-            direct: "https://gofile.io/d/bYr0O4    ",
-            mediafire: "https://gofile.io/d/bYr0O4    "
+            direct: "https://gofile.io/d/bYr0O4",
+            mediafire: "https://gofile.io/d/bYr0O4"
         },
         imagen: "Images/God_of_War_4_cover.jpg",
         password: "123"
     },
     {
-        id: 17,
+        id: 10,
         nombre: "God of War: Ragnarök",
         tipo: "juego",
         descripcion: "Kratos y Atreus se aventuran en Niflheim en busca de respuestas mientras se preparan para la profetizada batalla del Ragnarök.",
@@ -160,14 +160,14 @@ const recursos = [
         rating: "⭐⭐⭐⭐⭐",
         comments: ["La secuela épica", "Gráficos impresionantes"],
         links: {
-            direct: "https://gofile.io/d/bYr0O4    ",
-            mediafire: "https://gofile.io/d/bYr0O4    "
+            direct: "https://gofile.io/d/bYr0O4",
+            mediafire: "https://gofile.io/d/bYr0O4"
         },
         imagen: "Images/God_of_War_Ragnarök_cover.jpg",
         password: "123"
     },
     {
-        id: 18,
+        id: 11,
         nombre: "Peak",
         tipo: "juego",
         descripcion: "Un trepador experimentado se encuentra atrapado en una montaña nevada y debe encontrar la forma de sobrevivir y descender.",
@@ -176,14 +176,14 @@ const recursos = [
         rating: "⭐⭐⭐⭐☆",
         comments: ["Intenso y realista", "Una experiencia de supervivencia única"],
         links: {
-            direct: "https://gofile.io/d/2uRoh0  ",
-            mediafire: "https://gofile.io/d/2uRoh0  "
+            direct: "https://gofile.io/d/2uRoh0",
+            mediafire: "https://gofile.io/d/2uRoh0"
         },
         imagen: "Images/images.jpg",
         password: "123"
     },
     {
-        id: 19, // <--- ID ÚNICO para Sons of the Forest
+        id: 12,
         nombre: "Sons of the Forest",
         tipo: "juego",
         descripcion: "Un joven llega a una isla remota en busca de un multimillonario desaparecido, pero descubre que no está solo en esta experiencia de supervivencia aterradora.",
@@ -192,11 +192,61 @@ const recursos = [
         rating: "⭐⭐⭐⭐☆",
         comments: ["Terror psicológico y supervivencia", "Muy prometedor"],
         links: {
-            // NOTA: Reemplaza estos enlaces con enlaces válidos al juego
             direct: "https://www.mediafire.com/file/0vci1qnuuw6ms0q/Sons+Of+The+Forest+Opti.7z.torrent/file",
             mediafire: "https://www.mediafire.com/file/0vci1qnuuw6ms0q/Sons+Of+The+Forest+Opti.7z.torrent/file"
         },
-        imagen: "Images/Sons_of_the_Forest.jpg", // Asegúrate de tener esta imagen
-        password: "123" // Reemplaza si es necesario
+        imagen: "Images/Sons_of_the_Forest.jpg",
+        password: "123"
+    },
+
+    // ====== NUEVOS JUEGOS AÑADIDOS ======
+
+    {
+        id: 13,
+        nombre: "Deltarune",
+        tipo: "juego",
+        descripcion: "Secuela espiritual de Undertale. Un nuevo viaje lleno de personajes memorables, combate por turnos y decisiones morales.",
+        requisitos: "SO: Windows 7 o superior<br>Procesador: 1.6 GHz Dual Core<br>Memoria: 2 GB RAM<br>Gráficos: Tarjeta 3D compatible con DirectX 9<br>Almacenamiento: 1 GB",
+        downloads: 0,
+        rating: "⭐⭐⭐⭐⭐",
+        comments: ["Hermoso y emotivo", "Perfecto para fans de Undertale"],
+        links: {
+            direct: "https://gofile.io/d/deltarune",
+            mediafire: "https://www.mediafire.com/file/deltarune"
+        },
+        imagen: "Images/deltarune.jpg",
+        password: "deltarune"
+    },
+    {
+        id: 14,
+        nombre: "LEGO Marvel Super Heroes",
+        tipo: "juego",
+        descripcion: "Aventura de acción y comedia con tus héroes y villanos favoritos del Universo Marvel en versión LEGO.",
+        requisitos: "SO: Windows 7 / 8 / 10<br>Procesador: Intel Core 2 Quad Q8400 / AMD Phenom II X4 945<br>Memoria: 4 GB RAM<br>Gráficos: NVIDIA GeForce GT 630 / AMD Radeon HD 6570 (1 GB VRAM)<br>Almacenamiento: 15 GB",
+        downloads: 0,
+        rating: "⭐⭐⭐⭐☆",
+        comments: ["Divertido para toda la familia", "Lleno de referencias"],
+        links: {
+            direct: "https://gofile.io/d/lego-marvel",
+            mediafire: "https://www.mediafire.com/file/lego-marvel"
+        },
+        imagen: "Images/lego_marvel_superheroes.jpg",
+        password: "123"
+    },
+    {
+        id: 15,
+        nombre: "Call of Duty: Black Ops 2",
+        tipo: "juego",
+        descripcion: "Shooter futurista con campañas ramificadas y multijugador intenso, ambientado entre 1986 y 2025.",
+        requisitos: "SO: Windows 7 / 8 / 10 64-bit<br>Procesador: Intel Core i3-530 / AMD Phenom II X4 810<br>Memoria: 6 GB RAM<br>Gráficos: NVIDIA GeForce GTX 470 / AMD Radeon HD 6970 (1 GB VRAM)<br>Almacenamiento: 25 GB",
+        downloads: 0,
+        rating: "⭐⭐⭐⭐⭐",
+        comments: ["Uno de los mejores CoD", "Campaña con múltiples finales"],
+        links: {
+            direct: "https://gofile.io/d/blackops2",
+            mediafire: "https://www.mediafire.com/file/blackops2"
+        },
+        imagen: "Images/black_ops_2_cover.jpg",
+        password: "123"
     }
 ];
