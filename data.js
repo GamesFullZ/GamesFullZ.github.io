4a44eabdcdd2f97eed3b181c5e4c67907fb955cd
Games <gsrtitk@gmail.com>
2025-08-07 23:38:30 -0300

    Update data.js

--- data.js
+++ data.js
@@ -163,7 +163,7 @@ const recursos = [
             direct: "https://gofile.io/d/bYr0O4",
             mediafire: "https://gofile.io/d/bYr0O4"
         },
-        imagen: "Images/Images/God_of_War_Ragnarök_cover.jpg",
+        imagen: "Images/God_of_War_Ragnarök_cover.jpg",
         password: "123"
     },
     {
