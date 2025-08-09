83ac5de5888f0fd1fa3003a8ba7b9f8855d3583b
Games <gsrtitk@gmail.com>
2025-08-09 19:19:47 -0300

    Create sistemas.js

---
+++ sistemas.js
@@ -0,0 +1,8 @@
+// sistemas.js
+window.sistemas = [
+    {
+        nombre: "Windows 11",
+        imagen: "https://i.imgur.com/9mYgKl2.png",
+        tarjeta: "Compatibilidad total con más del 90% de juegos PC",
+        link: "https://www.microsoft.com/windows"
+    },
