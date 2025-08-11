2d5207d23a80ecfdb7c3620da6498c91f8a58dba
Games <gsrtitk@gmail.com>
2025-08-11 01:44:34 -0300

    Update sistemas.js

--- sistemas.js
+++ sistemas.js
@@ -4,5 +4,4 @@ const sistemas = [
         descripcion: "Versión personalizada de Windows 10 con mejoras de rendimiento",
         enlace: "https://www.microsoft.com/windows/get-windows-11",
         imagen: "Images/spectre.jpg"  // Ruta a tu imagen local
-    },
 ];
