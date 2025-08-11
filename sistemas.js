2eec78d358989ed2852bf631223dd0b7477d64c5
Games <gsrtitk@gmail.com>
2025-08-11 01:43:06 -0300

    Update sistemas.js

--- sistemas.js
+++ sistemas.js
@@ -5,16 +5,4 @@ const sistemas = [
         enlace: "https://www.microsoft.com/windows/get-windows-11",
         imagen: "Images/spectre.jpg"  // Ruta a tu imagen local
     },
-    {
-        nombre: "Ubuntu 22.04 LTS",
-        descripcion: "Distribución Linux popular y fácil de usar",
-        enlace: "https://ubuntu.com/download/desktop",
-        imagen: "Images/ubuntu.png"  // Ruta a tu imagen local
-    },
-    {
-        nombre: "macOS Ventura",
-        descripcion: "Sistema operativo oficial de Apple para Mac",
-        enlace: "https://www.apple.com/macos/ventura/",
-        imagen: "Images/macos.png"  // Ruta a tu imagen local
-    }
 ];
