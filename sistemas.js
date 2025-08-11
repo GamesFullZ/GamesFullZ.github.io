42200bc2d106e83bd2625e324290d0be750df8d5
Games <gsrtitk@gmail.com>
2025-08-11 01:25:18 -0300

    Create sistemas.js

---
+++ sistemas.js
@@ -0,0 +1,8 @@
+const sistemas = [
+    {
+        nombre: "Windows 11",
+        descripcion: "Versión más reciente de Windows",
+        enlace: "https://microsoft.com/windows11",
+        icono: "fab fa-windows"
+    },
+    {
