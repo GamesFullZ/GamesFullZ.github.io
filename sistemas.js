37dbbef345875f5622c206aea53e56e24d2b44c2
Games <gsrtitk@gmail.com>
2025-08-11 02:02:48 -0300

    Update sistemas.js

--- sistemas.js
+++ sistemas.js
@@ -7,13 +7,13 @@ const sistemas = [
     },
     {
         nombre: "Windows 7 Optios",
-        descripcion: "Distribución Linux popular y fácil de usar",
-        enlace: "https://ubuntu.com/download/desktop",
+        descripcion: "Windows 7 ultra optimizado",
+        enlace: "https://www.mediafire.com/file/ihyqrze1fktofe8/OptiOS+7+x64+V1.2.iso.torrent/file",
         imagen: "Images/ubuntu.png"  // Ruta a tu imagen local
     },
     {
         nombre: "Windows 10 LTSB Optios",
-        descripcion: "Sistema operativo oficial de Apple para Mac",
+        descripcion: "Sistema Modificado para consumir menos de 1gb ram en windows 10",
         enlace: "https://www.mediafire.com/file/f8k9a2ntc09rgzs/OptiOS+10+LTSB+x64+V2.3.iso.torrent/file",
         imagen: "Images/macos.png"  // Ruta a tu imagen local
     }
