ecbb2bda713979a9d30ccc6c25759b076558c6ce
ANIX <rodrigueztrianabrayanstiven@gmail.com>
2025-09-21 16:12:34 -0500

    Update data.js

--- data.js
+++ data.js
@@ -2159,7 +2159,7 @@ imagen: "Images/nig.webp",
     descripcion: "RPG de acción en tercera persona desarrollado por FromSoftware. Ambientado en un oscuro mundo de fantasía medieval, los jugadores asumen el papel del 'Unkindled' en su misión por enlazar la Llama. Con combates desafiantes, jefes imponentes y una atmósfera opresiva, Dark Souls 3 combina exploración, estrategia y precisión en cada enfrentamiento.",
     requisitos: "Windows 7 SP1 64bit, Procesador Intel Core i3-2100 / AMD FX-6300, 4 GB RAM, NVIDIA GeForce GTX 750 Ti / ATI Radeon HD 7950, 25 GB espacio",
     downloads: 0,
-    rating: "?????",
+    rating: "⭐⭐⭐⭐⭐",
     comments: [
         "difícil pero increíblemente satisfactorio",
         "la ambientación es brutal, cada jefe es una obra de arte"
